82d5574b316adeb256a0374b371bd8b6c2a4f5b9
Neo Ighodaro <neo.ighodaro@aboutyou/de>
2020-08-26 14:17:40 +0200

    Small fix

--- pages/posts/demo.js
+++ pages/posts/demo.js
@@ -1,13 +1,12 @@
-import PostLayout from '../../_layouts/post'
-import { getPostBySlug, getAllPosts } from "../../api"
- 
-export default function Demo(props) {
-  return <PostLayout title={props.title} content={props.content} meta={props.meta} />
-}
+import PostLayout from "../../_layouts/post";
+import { getPostBySlug, getAllPosts } from "../../api";
 
+export default function Demo(props) {
+  return <PostLayout title={props.title} content={props.content} meta={props.meta} />;
+}
 
 export async function getStaticProps(context) {
   return {
-    props: await getPostBySlug("1-remaking-my-website-again")
-  }
+    props: await getPostBySlug("1-spoke-laravel-worldwide-meetup-2020"),
+  };
 }
