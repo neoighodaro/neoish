64993c0ac2f8ff2b8d4084850402dc20409f6676
Neo Ighodaro <neo@creativitykills.co>
2021-02-22 20:36:22 +0100

    small update

--- _layouts/default.js
+++ _layouts/default.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import MetaTags from "react-meta-tags";
 
 export default function DefaultLayout({ description, title, children, path, image }) {
   let jsonLd = {
@@ -23,23 +22,21 @@ export default function DefaultLayout({ description, title, children, path, imag
   return (
     <main>
       <Head>
-        <MetaTags>
-          <title>{title}</title>
-          <meta name="description" content={description} />
-          <meta name="description" content={description} />
-          <meta name="name" content={title} />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content={title} />
-          <meta name="twitter:description" content={description} />
-          <meta name="twitter:site" content="@neoighodaro" />
-          <meta name="twitter:creator" content="@neoighodaro" />
-          <meta property="og:url" content={"https://neoighodaro.com" + (path || "/")} />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content={title} />
-          <meta property="og:description" content={description} />
-          <meta property="og:image" content={image || "https://neoighodaro.com/assets/neo.jpg"} />
-          <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: `${jsonLd}` }} />
-        </MetaTags>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="description" content={description} />
+        <meta name="name" content={title} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:site" content="@neoighodaro" />
+        <meta name="twitter:creator" content="@neoighodaro" />
+        <meta property="og:url" content={"https://neoighodaro.com" + (path || "/")} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image || "https://neoighodaro.com/assets/neo.jpg"} />
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: `${jsonLd}` }} />
       </Head>
 
       <div className="bg-indigo-800 h-1 md:h-2 w-full pin-t pin-l fixed"></div>
