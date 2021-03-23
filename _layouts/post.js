09e2778c6616980fb2044ea571e2cdd7e3912392
Neo Ighodaro <neo@creativitykills.co>
2021-03-23 08:51:29 +0100

    updated clap post title

--- _layouts/post.js
+++ _layouts/post.js
@@ -63,7 +63,7 @@ export default function PostLayout({ title, content, meta }) {
       <article className="max-w-full prose lg:prose-lg dark-mode:prose-dark">
         <h1>{title}</h1>
         <div class="mb-4" dangerouslySetInnerHTML={{ __html: content }} />
-        <blogstreak-clap></blogstreak-clap>
+        <blogstreak-clap title={title}></blogstreak-clap>
         <div className="mt-10">
           <Link href="/">
             <a>Home</a>
