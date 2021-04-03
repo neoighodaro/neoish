1bb5054d1f0c445ce69a54bcf5bec2639e4c0c74
Neo Ighodaro <neo@creativitykills.co>
2021-04-03 10:32:01 +0200

    Update link at bottom of posts to go to posts not home

--- _layouts/post.js
+++ _layouts/post.js
@@ -67,11 +67,22 @@ export default function PostLayout({ title, content, meta }) {
 
       <article className="max-w-full prose lg:prose-lg dark-mode:prose-dark">
         <h1>{title}</h1>
+
         <div className="mb-4" dangerouslySetInnerHTML={{ __html: content }} />
+
         <blogstreak-clap title={title}></blogstreak-clap>
+
         <div className="mt-6 py-4">
-          <Link href="/">
-            <a className="text-white no-underline">Home</a>
+          <Link href="/posts">
+            <a className="-mt-px pr-1 inline-flex items-center leading-5 font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-400 transition ease-in-out duration-150 no-underline">
+              <svg className="mr-3 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                <path
+                  fill-rule="evenodd"
+                  d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+                  clip-rule="evenodd"></path>
+              </svg>
+              Posts
+            </a>
           </Link>
         </div>
       </article>
