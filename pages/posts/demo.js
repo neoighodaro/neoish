5a09604e078148a160181c1fdabe746f0e4e2625
Neo Ighodaro <neo@creativitykills.co>
2021-04-03 12:24:13 +0200

    Netlify build fail fix

--- pages/posts/demo.js
+++ pages/posts/demo.js
@@ -1,12 +1,18 @@
 import PostLayout from "../../_layouts/post";
-import { getPostBySlug, getAllPosts } from "../../api";
+import { getPostBySlug, getAllPosts, getConfig } from "../../api";
 
-export default function Demo(props) {
-  return <PostLayout title={props.title} content={props.content} meta={props.meta} />;
+export default function Post(props) {
+  return <PostLayout title={props.title} content={props.content} meta={props.meta} config={props.config} />;
 }
 
 export async function getStaticProps(context) {
+  let config = await getConfig();
+  let postProps = await getPostBySlug("1-spoke-laravel-worldwide-meetup-2020");
+
   return {
-    props: await getPostBySlug("1-spoke-laravel-worldwide-meetup-2020"),
+    props: {
+      config,
+      ...postProps,
+    },
   };
 }
