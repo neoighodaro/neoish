457b6f14c90e36320339771a349e5e266b34435a
Neo Ighodaro <807318+neoighodaro@users.noreply.github.com>
2022-04-20 14:01:29 +0200

    Theme fixes

--- components/themeContext.js
+++ components/themeContext.js
@@ -1,17 +1,17 @@
 import React, { createContext, useState } from "react";
 
 const getInitialTheme = (_) => {
-  if (typeof window !== "undefined" && window.localStorage) {
-    // const storedPrefs = window.localStorage.getItem("color-theme");
-    // if (typeof storedPrefs === "string") {
-    //   return storedPrefs;
-    // }
+  // if (typeof window !== "undefined" && window.localStorage) {
+  // const storedPrefs = window.localStorage.getItem("color-theme");
+  // if (typeof storedPrefs === "string") {
+  //   return storedPrefs;
+  // }
 
-    const userMedia = window.matchMedia("(prefers-color-scheme: dark)");
-    if (userMedia.matches) {
-      return "dark";
-    }
+  const userMedia = window.matchMedia("(prefers-color-scheme: dark)");
+  if (userMedia.matches) {
+    return "dark";
   }
+  // }
 
   // If you want to use light theme as the default,
   // return "light" instead
@@ -30,7 +30,7 @@ export const ThemeProvider = ({ initialTheme, children }) => {
     root.classList.remove(isDark ? "light" : "dark");
     root.classList.add(theme);
 
-    localStorage.setItem("color-theme", theme);
+    // localStorage.setItem("color-theme", theme);
   };
 
   if (initialTheme) {
